066de67cc5d5935125df609e53d9c1fad6a83934
devtechsolution <srivastvadevzone@gmail.com>
2019-10-19 13:52:04 +0530

    Final commit

---
+++ rest-api-helper/src/test/java/com/api/rest/api/restassuredhelper/BaseClass.java
@@ -0,0 +1,15 @@
+package com.api.rest.api.restassuredhelper;
+
+import org.junit.BeforeClass;
+import static io.restassured.RestAssured.*;
+
+public class BaseClass {
+	
+	@BeforeClass
+	public static void setUp() {
+		baseURI = "http://localhost";
+		port = 8080;
+		basePath = "/laptop-bag/webapi/api";
+	}
+
+}
